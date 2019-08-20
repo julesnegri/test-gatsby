468949c7453ea1dcf6b648d2fa7747d547a729cb
soaresandrie <jsoaresandrie@gmail.com>
2019-08-19 22:46:13 -0300

    Criamos o component headerCubo.

---
+++ src/components/HeaderCubo/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+import "./styles.css"
+
+const HeaderCubo = ({ children }) => 
+<header className="header-cubo">
+    {children}
+</header>;
+
+export default HeaderCubo
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from "react"
 
 import SEO from "../components/seo"
+import HeaderCubo from "../components/HeaderCubo";
 
 const IndexPage = () => 
 <>
   <SEO title="Home" />
-  <h1>Home</h1>
+  <HeaderCubo> 
+    <h1>Hello</h1>
+  </HeaderCubo>
 </>
 
 
